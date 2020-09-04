db52754975a5c6b85920904987eed604f71c7532
Samiru73 <64921940+Samiru73@users.noreply.github.com>
2020-09-03 23:56:38 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,9 @@
+ScrollReveal().reveal('header');
+ScrollReveal().reveal('img');
+ScrollReveal().reveal('article');
+
+window.onload = function() 
+ {
+     $("#onload").fadeOut();
+     $("body").removeClass("hidde");
+}
